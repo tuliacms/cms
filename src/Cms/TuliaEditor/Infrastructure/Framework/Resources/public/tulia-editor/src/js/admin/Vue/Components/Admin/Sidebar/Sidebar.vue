335451775d0cd2f955e90d66164076a780fca0d8
Adam Banaszkiewicz <adam@tuliacms.com>
2022-03-28 08:04:39 +0200

    Styles

--- src/Cms/TuliaEditor/Infrastructure/Framework/Resources/public/tulia-editor/src/js/admin/Vue/Components/Admin/Sidebar/Sidebar.vue
+++ src/Cms/TuliaEditor/Infrastructure/Framework/Resources/public/tulia-editor/src/js/admin/Vue/Components/Admin/Sidebar/Sidebar.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="tued-sidebar">
         <div class="tued-sidebar-inner">
-            <div class="tued-controls-group">
-                <button type="button" class="tued-btn" @click="$root.$emit('editor.save')">Zapisz</button>
-                <button type="button" class="tued-btn" @click="$root.$emit('editor.cancel')">Anuluj</button>
+            <div class="tued-sidebar-toolbar">
+                <div class="tued-menu-holder">
+                    <div class="tued-menu-hamburger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <button type="button" class="tued-main-btn tued-btn-default" @click="$root.$emit('editor.cancel')">Porzuć</button>
+                <button type="button" class="tued-main-btn tued-btn-success" @click="$root.$emit('editor.save')">Zapisz</button>
             </div>
             <Structure :structure="structure" :messenger="messenger"></Structure>
         </div>
