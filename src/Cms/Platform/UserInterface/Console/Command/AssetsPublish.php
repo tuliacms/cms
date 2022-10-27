baef0d18151f66c6523d094c45700726788567a1
Adam Banaszkiewicz <adam@tuliacms.com>
2022-10-27 20:33:57 +0200

    Suuport quiet mode for assets publishing

--- src/Cms/Platform/UserInterface/Console/Command/AssetsPublish.php
+++ src/Cms/Platform/UserInterface/Console/Command/AssetsPublish.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tulia\Cms\Platform\UserInterface\Console\Command;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,10 +13,9 @@ use Tulia\Cms\Platform\Application\Service\AssetsPublisher;
 /**
  * @author Adam Banaszkiewicz
  */
+#[AsCommand(name: 'assets:publish', description: 'Publish assets to /public directory.')]
 class AssetsPublish extends Command
 {
-    protected static $defaultName = 'assets:publish';
-
     private AssetsPublisher $assetsPublisher;
     private array $assetsPublicPaths;
     private string $rootDir;
@@ -29,21 +29,22 @@ class AssetsPublish extends Command
         parent::__construct(self::$defaultName);
     }
 
-    protected function configure()
-    {
-        $this->setDescription('Publish assets to /public directory.');
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $quiet = $input->getOption('quiet');
+
         foreach ($this->assetsPublicPaths as $path) {
             $sourceHumanized = str_replace($this->rootDir, '', $path['source']);
             $targetHumanized = '/public/assets' . $path['target'];
 
-            if ($this->assetsPublisher->publish($path['source'], $path['target'])) {
-                $output->writeln(sprintf('<info>%s => %s</info>', $sourceHumanized, $targetHumanized));
-            } else {
-                $output->writeln(sprintf('<fg=red>%s => %s</>', $sourceHumanized, $targetHumanized));
+            $status = $this->assetsPublisher->publish($path['source'], $path['target']);
+
+            if (!$quiet) {
+                if ($status) {
+                    $output->writeln(sprintf('<info>%s => %s</info>', $sourceHumanized, $targetHumanized));
+                } else {
+                    $output->writeln(sprintf('<fg=red>%s => %s</>', $sourceHumanized, $targetHumanized));
+                }
             }
         }
 
